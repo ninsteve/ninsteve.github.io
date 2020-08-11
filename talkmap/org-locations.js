e8a38f3c6fbe65a2a3eeda9aaa9743b890f107e8
Steffen Werner <werner@mpi-magdeburg.mpg.de>
2020-08-11 13:03:40 +0200

    Reduced to Magdeburg and a single test location.

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,27 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
-  [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
-  ],
-  [
-    "Test location",
+    "Magdeburg, DE",
     52.131672,
     11.640320
-  ]
+  ],
+  [
+    "Test",
+    52.131672,
+    11.640320
+  ]  
 ];
